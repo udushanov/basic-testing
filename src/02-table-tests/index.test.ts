6991589bf882c62e21251f0c92858a6e0af18b2c
Ulugbek <u.dushanov@gmail.com>
2024-01-28 22:25:00 +0500

    feat: add solution to task 02-table-tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,28 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  { a: 5, b: 2, action: Action.Subtract, expected: 3 },
+  { a: 8, b: 3, action: Action.Subtract, expected: 5 },
+  { a: 4, b: 3, action: Action.Multiply, expected: 12 },
+  { a: 2, b: 5, action: Action.Multiply, expected: 10 },
+  { a: 8, b: 2, action: Action.Divide, expected: 4 },
+  { a: 15, b: 3, action: Action.Divide, expected: 5 },
+  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+  { a: 4, b: 0.5, action: Action.Exponentiate, expected: 2 },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
+  test('should correctly calculate results for various cases', () => {
+    testCases.forEach(({ a, b, action, expected }) => {
+      const input = { a, b, action };
+
+      const result = simpleCalculator(input);
+
+      expect(result).toBe(expected);
+    });
     expect(true).toBe(true);
   });
-  // Consider to use Jest table tests API to test all cases above
 });
